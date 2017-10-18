667b00bdf4d57b33e94159ab8a527d399efa9769
Santhosh Gugan <santhoshg1990@gmail.com>
2017-10-18 23:18:04 +0530

    #work1 #sidebar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,28 @@
 // Custom Javascript
+/*Work templates in order*/
+var workTemplates = ["work1","work2"];
+$(document).ready(function(){
+	$('#forward').click(function(event) {
+		/* Act on the event */
+		var hrefs = window.location.href;
+		var path = window.location.pathname.split("/");
+		var file = path[path.length - 1];
+		var fileName = file.split(".")[0];
+		var index = 0;
+		for(var i=0;i<workTemplates.length;i++){
+			if(fileName == workTemplates[i]){
+				index = i;
+				break;
+			}
+		}
+
+		/*check if end of templagte is reached*/
+		if(index == workTemplates.length - 1){
+			$("#forward").addClass('disable');
+			return;
+		}
+		/*Increment index*/
+		index++
+		window.location.href = hrefs.replace(fileName, workTemplates[index]);
+	});
+});
